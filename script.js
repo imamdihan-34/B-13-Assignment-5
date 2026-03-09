466547342137e35971196664ab19a0cbf0c14eda
imamdihan-34 <imamdihan@gmail.com>
2026-03-09 06:34:10 +0600

    js code formatting

--- script.js
+++ script.js
@@ -1,90 +1,69 @@
 const issuesContainer = document.getElementById("issues-container");
 const loader = document.getElementById("loader");
-
 const allBtn = document.getElementById("allBtn");
 const openBtn = document.getElementById("openBtn");
 const closedBtn = document.getElementById("closedBtn");
-
 const searchInput = document.getElementById("searchInput");
 
 let allIssues = [];
 
-// load issues
 async function loadIssues() {
   loader.classList.remove("hidden");
-
   const res = await fetch(
     "https://phi-lab-server.vercel.app/api/v1/lab/issues",
   );
-
   const data = await res.json();
-
   allIssues = data.data;
-
   displayIssues(allIssues);
-
   loader.classList.add("hidden");
 }
+
 document.getElementById("openBtn").addEventListener("click", () => {
   loader.classList.remove("hidden");
 
   setTimeout(() => {
     const openIssues = allIssues.filter((issue) => issue.status === "open");
-
     displayIssues(openIssues);
-
     loader.classList.add("hidden");
   }, 200);
 });
+
 document.getElementById("closedBtn").addEventListener("click", () => {
   loader.classList.remove("hidden");
-
   setTimeout(() => {
     const closedIssues = allIssues.filter((issue) => issue.status === "closed");
-
     displayIssues(closedIssues);
-
     loader.classList.add("hidden");
   }, 200);
 });
 
 function displayIssues(issues) {
   issuesContainer.innerHTML = "";
-
   issues.forEach((issue) => {
     const borderColor =
       issue.status === "open"
         ? "border-t-4 border-green-500"
         : "border-t-4 border-purple-500";
-
     const statusIcon =
       issue.status === "open"
         ? `<img src="./assets/Open-Status.png" class="w-6">`
         : `<img src="../assets/Closed- Status .png" class="w-6">`;
 
     const card = document.createElement("div");
-
     card.className = `card bg-white shadow ${borderColor}`;
-
     card.innerHTML = `
-
 <div class="card-body">
-
 <div class="flex justify-between">
 <span>
 ${statusIcon}
 </span>
-
 <span class="text-xs px-3 py-1 rounded bg-red-100 text-red-500">
 ${issue.priority}
 </span>
-
 </div>
 
-
 <h2 class="card-title">${issue.title}</h2>
-
-<p class="text-sm line-clamp-2 text-slate-500">${issue.description}</p>
+  <p class="text-sm line-clamp-2 text-slate-500">${issue.description}</p>
 
 <div class="flex gap-2">
 
@@ -99,16 +78,12 @@ ${issue.labels
   )
   .join("")}
 
+  </div>
 
-</div>
-
-<div class="border-t pt-2 text-sm text-gray-500">
-
-<p>#${issue.id} by ${issue.author}</p>
-
-<p>${issue.createdAt}</p>
-
-</div>
+  <div class="border-t pt-2 text-sm text-gray-500">
+    <p>#${issue.id} by ${issue.author}</p>
+    <p>${issue.createdAt}</p>
+  </div>
 
 </div>
 
@@ -120,7 +95,6 @@ ${issue.labels
   });
 }
 document.getElementById("openBtn").addEventListener("click", () => {
-
 const openIssues = allIssues.filter(issue => issue.status === "open")
 
 displayIssues(openIssues)
